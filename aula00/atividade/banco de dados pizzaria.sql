20431ce290c1446ad9f7ae7e539096893feeae54
Henrique-RMotta <henrique2008790@gmail.com>
2025-08-01 16:44:19 -0300

    atividade

---
+++ aula00/atividade/banco de dados pizzaria.sql
@@ -0,0 +1,14 @@
+create database pizzaria;
+use pizzaria;
+
+create table pizza (
+id_pizza int primary key
+);
+
+create table funcionario (
+id_funcionario int primary key
+);
+
+create table cliente (
+id_cliente int primary key
+);
