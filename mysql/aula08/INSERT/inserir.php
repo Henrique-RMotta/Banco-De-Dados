5eabf345e26542b268bf3486f1c5867f6274b050
Henrique-RMotta <henrique2008790@gmail.com>
2025-10-31 16:56:31 -0300

    php

---
+++ mysql/aula08/INSERT/inserir.php
@@ -0,0 +1,21 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+$conn = new mysqli("localhost","root","senaisp","livraria");
+
+if ($conn->connect_error){
+    die("Erro de conexão". $conn->connect_error);
+}
+
+$sql = "INSERT INTO usuarios (nome,email) values (´$nome´,´$email´)";
+if($conn->query($sql) === TRUE){
+    echo "Dados salvos com sucesso";
+} else {
+    echo "Erro:". $conn->error;
+}
+
+// header("Location: index.html"); ou index.php, dependendo do seu arquivo principal
+//exit;
+//$conn ->close();
+?>
